bb1af82900a758bb869c73e1a08f3fcee2603297
Ashwin nair <nairashwin17@gmail.com>
2021-12-06 14:31:25 +0530

    crypto optiomization

--- src/app/crypto-details/crypto-details.page.ts
+++ src/app/crypto-details/crypto-details.page.ts
@@ -24,7 +24,7 @@ export class CryptoDetailsPage implements OnInit {
          text: "Percent change in price"
       },
       // subtitle: {
-      //    text: "Source: WorldClimate.com"
+      //    text: ""
       // },
       xAxis: {
          categories: ['3m', '2m', '1m', '1w', '1d', '1h']
@@ -72,24 +72,7 @@ export class CryptoDetailsPage implements OnInit {
             }
          }]
       },
-      series: [
-         // {
-         //    name: 'Tokyo',
-         //    data: [-53.22366617, -38.92618989, -32.95635107, -18.09961304, -12.6282003, 3.45679759]
-         // },
-         //  {
-         //     name: 'New York',
-         //     data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8,24.1, 20.1, 14.1, 8.6, 2.5]
-         //  },
-         //  {
-         //     name: 'Berlin',
-         //     data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-         //  },
-         //  {
-         //     name: 'London',
-         //     data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-         //  }
-      ]
+      series: []
    };
    public cryptoData: any[] = [];
    constructor(private route: ActivatedRoute, private router: Router) {
